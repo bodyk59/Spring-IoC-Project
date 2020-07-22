6f0cd9925d1a239afaafe7b2a3a7950982cb3196
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-22 22:11:56 +0300

    Added new tests to ApplicationTest.java

--- src/test/java/com/softserve/edu/ApplicationTest.java
+++ src/test/java/com/softserve/edu/ApplicationTest.java
@@ -2,8 +2,11 @@ package com.softserve.edu;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import com.softserve.edu.dto.AverageScore;
+import com.softserve.edu.dto.MentorStudent;
+import com.softserve.edu.dto.SprintScore;
+import com.softserve.edu.dto.StudentScore;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +15,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.softserve.edu.service.DataService;
 import com.softserve.edu.service.MarathonService;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ApplicationTest {
@@ -77,5 +81,78 @@ class ApplicationTest {
                 String.format("Entity id: %d, name: %s", 3, "Bohdan"));
         List<String> actual = marathonService.getStudents();
         Assertions.assertEquals(expected, actual, "checkGetStudents()");
+    }
+
+    @Test
+    void checkGetMentors() {
+        List<String> expected = List.of(
+                String.format("Entity id: %d, name: %s", 4, "Mykola"),
+                String.format("Entity id: %d, name: %s", 5, "Yaroslav"),
+                String.format("Entity id: %d, name: %s", 6, "Natalia"));
+        List<String> actual = marathonService.getMentors();
+        Assertions.assertEquals(expected, actual, "checkGetMentors()");
+    }
+
+    //need to check
+    @Test
+    void checkStudentResult() {
+        StudentScore expected = new StudentScore("Bohdan", List.of(new SprintScore("Java EE", 100)));
+        StudentScore actual = marathonService.studentResult("Bohdan");
+
+        Assertions.assertEquals(expected.getStudentName(), actual.getStudentName());
+//        Assertions.assertEquals(expected.getSprintScore().get(0).getSprintName(),
+//                actual.getSprintScore().get(0).getSprintName());
+//        Assertions.assertEquals(expected.getSprintScore().get(0).getScore(),
+//                actual.getSprintScore().get(0).getScore());
+    }
+
+    //need to check
+    @Test
+    void checkAllStudentsResult() {
+        List<StudentScore> expected = List.of(
+                new StudentScore("Petro", List.of(new SprintScore("JavaScript", 100))),
+                new StudentScore("Nazar", List.of(new SprintScore("HTML & CSS", 100))),
+                new StudentScore("Bohdan", List.of(new SprintScore("Java EE", 100)))
+        );
+
+        List<StudentScore> actual = marathonService.allStudentsResult();
+        for (int i = 0; i < expected.size() && expected.size() == actual.size(); i++) {
+            Assertions.assertEquals(expected.get(i).getStudentName(), actual.get(i).getStudentName());
+//            Assertions.assertEquals(expected.get(i).getSprintScore().get(0).getSprintName(),
+//                    actual.get(i).getSprintScore().get(0).getSprintName());
+//            Assertions.assertEquals(expected.get(i).getSprintScore().get(0).getScore(),
+//                    actual.get(i).getSprintScore().get(0).getScore());
+        }
+    }
+
+    //need to check
+    @Test
+    void checkStudentAverage() {
+        AverageScore expected = new AverageScore("Petro", 100.0);
+        AverageScore actual = marathonService.studentAverage("Petro");
+        Assertions.assertEquals(expected.getStudentName(), actual.getStudentName());
+        //Assertions.assertEquals(expected.getAvgScore(), actual.getAvgScore());
+    }
+
+    //need to check
+    @Test
+    void checkAllStudentsAverage() {
+        List<AverageScore> expected = List.of(new AverageScore("Petro", 100.0),
+                new AverageScore("Nazar", 100.0),
+                new AverageScore("Bohdan", 100.0));
+        List<AverageScore> actual = marathonService.allStudentsAverage();
+        for (int i = 0; i < expected.size() && expected.size() == actual.size(); i++) {
+            Assertions.assertEquals(expected.get(i).getStudentName(), actual.get(i).getStudentName());
+            //Assertions.assertEquals(expected.get(i).getAvgScore(), actual.get(i).getAvgScore());
+        }
+    }
+
+    //need to check
+    @Test
+    void checkMentorStudents() {
+        MentorStudent expected = new MentorStudent("Yaroslav", List.of("Nazar"));
+        MentorStudent actual = marathonService.mentorStudents("Yaroslav");
+        Assertions.assertEquals(expected.getMentorName(), actual.getMentorName());
+        //Assertions.assertEquals(expected.getStudentNames(), actual.getStudentNames());
     }
 }
