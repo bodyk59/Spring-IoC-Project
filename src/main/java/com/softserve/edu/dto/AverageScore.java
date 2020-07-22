f9b10ccadc933341ee4f54dfe3fd83edf5d87d9a
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-22 09:11:47 +0300

    Added realisation to AverageScore.java

--- src/main/java/com/softserve/edu/dto/AverageScore.java
+++ src/main/java/com/softserve/edu/dto/AverageScore.java
@@ -3,5 +3,25 @@ package com.softserve.edu.dto;
 public class AverageScore {
     private String studentName;
     private double avgScore;
-    // TODO
+
+    public AverageScore(String studentName, double avgScore) {
+        this.studentName = studentName;
+        this.avgScore = avgScore;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public double getAvgScore() {
+        return avgScore;
+    }
+
+    public void setAvgScore(double avgScore) {
+        this.avgScore = avgScore;
+    }
 }
