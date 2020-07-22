cf2c5360bd2075196a5388b84c506e2635afbf96
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-22 09:12:06 +0300

    Added realisation to SprintScore.java

--- src/main/java/com/softserve/edu/dto/SprintScore.java
+++ src/main/java/com/softserve/edu/dto/SprintScore.java
@@ -3,5 +3,25 @@ package com.softserve.edu.dto;
 public class SprintScore {
     private String sprintName;
     private int score;
-    // TODO
+
+    public SprintScore(String sprintName, int score) {
+        this.sprintName = sprintName;
+        this.score = score;
+    }
+
+    public String getSprintName() {
+        return sprintName;
+    }
+
+    public void setSprintName(String sprintName) {
+        this.sprintName = sprintName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
