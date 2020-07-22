344bf7ca84d8084baa89a6251e20bced16fb680c
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-22 17:49:51 +0300

    Added @Autowired, DataService to constructor, helpers methods for fillDataService and realization for fillDataService

--- src/test/java/com/softserve/edu/ApplicationTest.java
+++ src/test/java/com/softserve/edu/ApplicationTest.java
@@ -14,29 +14,68 @@ import com.softserve.edu.service.MarathonService;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class ApplicationTest {
+class ApplicationTest {
     
-    //@Autowired
-    private MarathonService marathonService;
+    @Autowired
+    private final MarathonService marathonService;
     
-    //Autowired
-    private DataService dataService;
+    @Autowired
+    private final DataService dataService;
 
     @Autowired
-    public ApplicationTest(MarathonService marathonService) {
+    public ApplicationTest(MarathonService marathonService, DataService dataService) {
         this.marathonService = marathonService;
+        this.dataService = dataService;
         fillDataService();
     }
-    
+
     private void fillDataService() {
-        // TODO for dataService
+        fillStudentsList(dataService);
+        fillMentorsList(dataService);
+        fillSprintsList(dataService);
+        fillSolutionsList(dataService);
+        fillCommunicationList(dataService);
     }
-    
+
+    //Filling DataService: start
+    private void fillStudentsList(DataService dataService) {
+        dataService.addStudent("Petro");
+        dataService.addStudent("Nazar");
+        dataService.addStudent("Bohdan");
+    }
+
+    private void fillMentorsList(DataService dataService) {
+        dataService.addMentor("Mykola");
+        dataService.addMentor("Yaroslav");
+        dataService.addMentor("Natalia");
+    }
+
+    private void fillSprintsList(DataService dataService) {
+        dataService.addSprint("1. Java EE");
+        dataService.addSprint("2. JavaScript");
+        dataService.addSprint("1. HTML & CSS");
+    }
+
+    private void fillSolutionsList(DataService dataService) {
+        dataService.addSolution("Bohdan", "Java EE", 100);
+        dataService.addSolution("Petro", "JavaScript", 100);
+        dataService.addSolution("Nazar", "HTML & CSS", 100);
+    }
+
+    private void fillCommunicationList(DataService dataService) {
+        dataService.addCommunication("Nazar", "Yaroslav");
+        dataService.addCommunication("Bohdan", "Mykola");
+        dataService.addCommunication("Petro", "Natalia");
+    }
+    //Filling DataService: stop
+
     @Test
-    public void checkGetStudents() {
-        List<String> expected = null;
+    void checkGetStudents() {
+        List<String> expected = List.of(
+                String.format("Entity id: %d, name: %s", 1, "Petro"),
+                String.format("Entity id: %d, name: %s", 2, "Nazar"),
+                String.format("Entity id: %d, name: %s", 3, "Bohdan"));
         List<String> actual = marathonService.getStudents();
         Assertions.assertEquals(expected, actual, "checkGetStudents()");
     }
-
 }
