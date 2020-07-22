a6d9f7029f9f6724dba5e2b43a15325292875cd3
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-22 07:32:06 +0300

    Added realisation to Entity.java

--- src/main/java/com/softserve/edu/entity/Entity.java
+++ src/main/java/com/softserve/edu/entity/Entity.java
@@ -3,5 +3,44 @@ package com.softserve.edu.entity;
 public class Entity {
     private int id; // must be unique
     private String name;
-    // TODO
+
+    public Entity(String name) {
+        ++this.id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Entity)) return false;
+
+        Entity entity = (Entity) o;
+
+        if (getId() != entity.getId()) return false;
+        return getName() != null ? getName().equals(entity.getName()) : entity.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Entity id: %d, name: %s", id, name);
+    }
 }
