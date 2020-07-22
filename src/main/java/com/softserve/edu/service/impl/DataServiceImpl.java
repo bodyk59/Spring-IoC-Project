1f3c44e6b024f32ecbde54218618e8078b777300
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-22 07:51:41 +0300

    Added realisation to DataServiceImpl.java

--- src/main/java/com/softserve/edu/service/impl/DataServiceImpl.java
+++ src/main/java/com/softserve/edu/service/impl/DataServiceImpl.java
@@ -15,26 +15,77 @@ public class DataServiceImpl implements DataService {
     private List<Solution> solution;
 
     public void addStudent(String studentName) {
-        // TODO for students
+        if (studentName != null) {
+            students.add(new Entity(studentName));
+        }
     }
 
     public void addMentor(String mentorName) {
-        // TODO for mentors
+        if (mentorName != null) {
+            mentors.add(new Entity(mentorName));
+        }
     }
 
     public void addSprint(String sprintName) {
-        // TODO for sprints
+        if (sprintName != null) {
+            sprints.add(new Entity(sprintName));
+        }
     }
 
     public void addCommunication(String studentName, String mentorName) {
-        // TODO for communication
+        int studentId = getIdOnName(students, studentName);
+        int mentorId = getIdOnName(mentors, mentorName);
+        if (studentId == 0 || mentorId == 0) {
+            //such student or mentor does not exist
+            //may be useful int the future
+            return;
+        }
+        communication.add(new Communication(studentId, mentorId));
     }
 
     public void addSolution(String studentName, String sprintName, int score) {
-        // TODO for solution
+        int studentId = getIdOnName(students, studentName);
+        int sprintId = getIdOnName(sprints, sprintName);
+        if (studentId == 0 || sprintId == 0) {
+            //such student or sprint does not exist
+            //may be useful int the future
+            return;
+        }
+        if (score <= 0 || score > 12) {
+            //base check
+            //may be useful int the future
+            return;
+        }
+        solution.add(new Solution(studentId, sprintId, score));
     }
-    
-    // getters
-    
-    // TODO
+
+    private int getIdOnName(List<Entity> list, String name) {
+        return list.stream()
+                .filter(entity -> entity.getName().equals(name))
+                .map(Entity::getId)
+                .findFirst()
+                .orElse(0);
+    }
+
+    public List<Entity> getStudents() {
+        return students;
+    }
+
+    public List<Entity> getMentors() {
+        return mentors;
+    }
+
+    public List<Entity> getSprints() {
+        return sprints;
+    }
+
+    public List<Communication> getCommunication() {
+        return communication;
+    }
+
+    public List<Solution> getSolution() {
+        return solution;
+    }
+
+    //Maybe something else?
 }
