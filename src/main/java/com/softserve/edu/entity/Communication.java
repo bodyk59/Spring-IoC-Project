72c4a4443200650cf05dd481d22900724370b47d
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-22 07:32:22 +0300

    Added realisation to Communication.java

--- src/main/java/com/softserve/edu/entity/Communication.java
+++ src/main/java/com/softserve/edu/entity/Communication.java
@@ -3,5 +3,40 @@ package com.softserve.edu.entity;
 public class Communication {
     private int idStudent;
     private int idMentor;
-    // TODO
+
+    public Communication(int idStudent, int idMentor) {
+        this.idStudent = idStudent;
+        this.idMentor = idMentor;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public int getIdMentor() {
+        return idMentor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Communication)) return false;
+
+        Communication that = (Communication) o;
+
+        if (getIdStudent() != that.getIdStudent()) return false;
+        return getIdMentor() == that.getIdMentor();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getIdStudent();
+        result = 31 * result + getIdMentor();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Communication idStudent: %d, idMentor: %d", idStudent, idMentor);
+    }
 }
