21d906e53c49e1e1d511c20a5eb5db106e631121
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-07-22 09:12:20 +0300

    Added realisation to StudentScore.java

--- src/main/java/com/softserve/edu/dto/StudentScore.java
+++ src/main/java/com/softserve/edu/dto/StudentScore.java
@@ -5,5 +5,25 @@ import java.util.List;
 public class StudentScore {
     private String studentName;
     private List<SprintScore> sprintScore;
-    // TODO
+
+    public StudentScore(String studentName, List<SprintScore> sprintScore) {
+        this.studentName = studentName;
+        this.sprintScore = sprintScore;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public List<SprintScore> getSprintScore() {
+        return sprintScore;
+    }
+
+    public void setSprintScore(List<SprintScore> sprintScore) {
+        this.sprintScore = sprintScore;
+    }
 }
